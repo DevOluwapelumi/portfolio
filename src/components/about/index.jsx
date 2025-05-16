30880ce2b9cb1220fa00ebc7969c22a98d909f24
Victor Oluwapelumi <victoroluwadarasimi16@gmail.com>
2025-05-16 13:00:11 +0100

    initial commit

---
+++ src/components/about/index.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import ItemLayout from "./ItemLayout";
+import Link from "next/link";
+
+const AboutDetails = () => {
+  return (
+    <section className="py-20 w-full">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
+        <ItemLayout
+          className={
+            " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
+          }
+        >
+         <h2 className="text-xl md:text-2xl text-left w-full capitalize">
+  Software Developer || Social Media Manager
+</h2>
+          
+<div className="font-light text-xs sm:text-sm md:text-base gap-3 flex flex-col w-full">
+  <p>I’m a passionate Software Developer and Social Media Manager with a unique blend of technical expertise and digital storytelling. In the development space, I specialize in building intuitive, high-performing web applications using JavaScript, React.js, and Next.js. My work focuses on creating user-centric digital experiences that are fast, scalable, and visually engaging.</p>
+
+  <p>On the other side of the screen, I craft compelling content strategies, manage online communities, and leverage analytics to drive brand growth across social platforms. </p>
+
+  <p>Whether I’m writing code or curating content, my goal is the same: to build meaningful connections through technology and creativity.</p>
+</div>
+
+        </ItemLayout>
+
+        <ItemLayout
+          className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            25+ <sub className="font-semibold text-base">clients</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            2+{" "}
+            <sub className="font-semibold text-base">years of experience</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
+        >
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full"}>
+          <img
+            className="w-full h-auto"
+            src={`https://skillicons.dev/icons?i=appwrite,aws,babel,bootstrap,cloudflare,css,d3,docker,figma,firebase,gatsby,git,github,graphql,html,ipfs,js,jquery,kubernetes,linux,mongodb,mysql,netlify,nextjs,nodejs,npm,postgres,react,redux,replit,sass,supabase,tailwind,threejs,vercel,vite,vscode,yarn`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=codebucks27&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <Link
+            href="https://github.com/codebucks27/Nextjs-contentlayer-blog"
+            target="_blank"
+            className="w-full"
+          >
+            <img
+              className="w-full h-auto"
+              src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/pin/?username=codebucks27&repo=Nextjs-contentlayer-blog&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
+              alt="CodeBucks"
+              loading="lazy"
+            />
+          </Link>
+        </ItemLayout>
+      </div>
+    </section>
+  );
+};
+
+export default AboutDetails;
